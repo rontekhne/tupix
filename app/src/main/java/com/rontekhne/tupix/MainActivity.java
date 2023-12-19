8dbfbf39f30e69cbbf2eadd3444ee521ef10c1db
ron <rontekhne@users.noreply.github.com>
2023-12-19 17:36:30 -0300

    updated layout

--- app/src/main/java/com/rontekhne/tupix/MainActivity.java
+++ app/src/main/java/com/rontekhne/tupix/MainActivity.java
@@ -1,17 +1,24 @@
 package com.rontekhne.tupix;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.method.ScrollingMovementMethod;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.util.Xml;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
         textResult.setMovementMethod(new ScrollingMovementMethod());
     }
 
-    // adicionar estilo à saída
     public void search(View view) {
+        // scroll to the top when search button is clicked
+        textResult.scrollTo(0, 0);
+
         // hide keyboard
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         if (inputMethodManager != null) {
@@ -42,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         searchWord = textInput.getText().toString().toLowerCase();
 
-        StringBuilder resultText = new StringBuilder();
+        SpannableStringBuilder resultText = new SpannableStringBuilder();
 
         try {
             // read the XML file
@@ -82,18 +91,37 @@ public class MainActivity extends AppCompatActivity {
                                         ("a".equals(parser.getName()) ||
                                                 "b".equals(parser.getName()))) {
                                     // end of tags , get out of inner loop
-                                    // ** add all letters here manually or in a loop
                                     break;
                                 }
                                 eventType = parser.next();
                             }
 
-                            // add to the string of results
-                            String resultEntry = "Palavra: " + word +
-                                    "\nClasse: " + clazz +
-                                    "\nSignificado: " + meaning +
-                                    "\nFontes: " + source + "\n\n";
-                            resultText.append(resultEntry);
+                            // format the lines
+                            String fclazz = "\n\n" + clazz;
+                            String fmeaning = "\n\n" + meaning;
+                            String fsource = "\n\nFontes:\n" + source + "\n\n\n";
+
+                            String resultEntry = word + fclazz + fmeaning + fsource;
+
+                            // apply style to the lines
+                            SpannableString formattedResultEntry = new SpannableString(resultEntry);
+                            formattedResultEntry.setSpan(new RelativeSizeSpan(1.8f), 0, word.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            formattedResultEntry.setSpan(new StyleSpan(Typeface.BOLD), 0, word.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            int classStart = word.length();
+                            int classEnd = classStart + fclazz.length();
+                            formattedResultEntry.setSpan(new RelativeSizeSpan(1.2f), classStart, classEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                            formattedResultEntry.setSpan(new StyleSpan(Typeface.ITALIC), classStart, classEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            int meaningStart = classEnd;
+                            int meaningEnd = meaningStart + fmeaning.length();
+                            formattedResultEntry.setSpan(new RelativeSizeSpan(1.2f), meaningStart, meaningEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            int sourceStart = meaningEnd;
+                            int sourceEnd = sourceStart + fsource.length();
+                            formattedResultEntry.setSpan(new RelativeSizeSpan(0.8f), sourceStart, sourceEnd, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            resultText.append(formattedResultEntry);
                         }
                     }
                 }
@@ -102,13 +130,13 @@ public class MainActivity extends AppCompatActivity {
 
             if (resultText.length() > 0) {
                 // show results in the TextView
-                // resultText.insert(0, "Resultados para " + searchWord + ":\n");
-                textResult.setText(resultText.toString());
-            } else {
+                textResult.setText(resultText);
+            } else { // word doesn't exit in the tupix.xml
                 textResult.setText("Palavra não encontrada no dicionário.");
             }
-        } catch (Exception e) {
+        } catch (XmlPullParserException | IOException e) {
             e.printStackTrace();
+            Toast.makeText(this, "Erro ao processar o arquivo XML.", Toast.LENGTH_SHORT).show();
         }
 
         // clean the input
